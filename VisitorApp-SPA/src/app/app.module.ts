30fb74c9b25c761fec64d59e93b50e1f3efc91f7
xlnd47 <axel.deschuijmer@gmail.com>
2020-11-18 19:01:21 +0100

    Error Handeling

--- VisitorApp-SPA/src/app/app.module.ts
+++ VisitorApp-SPA/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -9,23 +9,18 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-      NavComponent,
-      HomeComponent,
-      RegisterComponent,
-      CheckoutComponent
-   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    CheckoutComponent,
   ],
-  providers: [
-    AuthService
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [AuthService, ErrorInterceptorProvider],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
