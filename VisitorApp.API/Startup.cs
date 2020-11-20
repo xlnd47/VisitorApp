838e2f9d2df0f5b9223eff94a91f57b13ca06806
xlnd47 <axel.deschuijmer@gmail.com>
2020-11-20 05:21:08 +0100

    build ready

--- VisitorApp.API/Startup.cs
+++ VisitorApp.API/Startup.cs
@@ -89,12 +89,16 @@ namespace VisitorApp.API
             app.UseRouting();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
